9a345bce8d08221daf8338c1a3dfd5f511c29585
harshithaguthi13-debug <harshithaguthi13@gmail.com>
2025-10-16 10:28:43 +0530

    Create  illustrates the use of inline functions .cpp

---
+++ illustrates the use of inline functions .cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+inline int square(int x) 
+{ 
+    return x * x; 
+} 
+int main()  
+{ 
+     cout << "Roll No: 24B11AI138" << endl; 
+    int num = 4; 
+    cout << "Square of " << num << " is " << square(num) << endl; 
+    return 0; 
+} 
